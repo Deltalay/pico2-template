00adc1cf18212bfe8a2c36bc336e2f1c8e065f7f
Deltalay <88176924+Deltalay@users.noreply.github.com>
2026-08-28 09:50:34 +0700

    Add GPIO operation such as gpio_init, gpio_set, gpio_out and gpio_read gpio_init use for initlize the GPIO and the select the function gpio_set use for setting GPIO as input or output gpio_out use for setting gpio high or low gpio_read use for readding gpio state

--- header/gpio.h
+++ header/gpio.h
@@ -1,7 +1,30 @@
 
 #ifndef GPIO_H
 #define GPIO_H
+
 #include <stdint.h>
-void gpio_init(uint16_t pin_number);
-void blink_BLINK();
+typedef enum {
+    LOW = 0,
+    HIGH = 1
+} rp2350_pin_state_t;
+typedef enum {
+    JTAG = 0,
+    SPIO = 1,
+    UART = 2,
+    I2C = 3,
+    PWM = 4,
+    SIO = 5,
+    PIO0 = 6,
+    PIO1 = 7,
+    PIO2 = 8
+} rp2350_gpio_func_t;
+typedef enum {
+    INPUT = 0,
+    OUTPUT = 1,
+}rp2350_gpio_direction_t;
+void gpio_init(uint8_t pin_number, rp2350_gpio_func_t function);
+void gpio_set(uint8_t pin_number, rp2350_gpio_direction_t direction);
+void gpio_out(uint8_t pin_number, rp2350_pin_state_t state);
+rp2350_pin_state_t gpio_read(uint8_t pin_number);
+
 #endif
